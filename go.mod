8bf24fc3a015913dd0d082ae7701563ab9c06b12
Tanu Chaurasiya <tanuchaurasiya@Tanus-MacBook-Air.local>
2023-10-02 17:40:41 +0530

    added different end points in web server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module web-server
+
+go 1.21.0
+
+require (
+	github.com/google/uuid v1.3.1
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1 style = 'color:steelblue'> hello world</h1>"))
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	book := []Book{
+		{
+			Title:  "this is title1",
+			Author: "this is author1",
+			Pages:  100},
+		{
+			Title:  "this is title2",
+			Author: "this is author2",
+			Pages:  200,
+		},
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
+	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello tanu"))
+		w.Write([]byte("hello from main"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/getbook", getBook)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
